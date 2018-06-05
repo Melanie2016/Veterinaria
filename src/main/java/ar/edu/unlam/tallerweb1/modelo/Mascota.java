540cc715658abfb2c9ce67cc2a4c5fba765f7939
nataliaeli <nataliacipollino@outlook.com>
2018-06-05 11:25:55 -0300

    se creo la clase mascota y su relacion con la clase usuario

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Mascota.java
@@ -0,0 +1,62 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Mascota {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private String tipo;
+	
+	
+	@ManyToOne
+	private Usuario dueño;
+
+	//getters y setters
+	
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public Usuario getDueño() {
+		return dueño;
+	}
+
+
+	public void setDueño(Usuario dueño) {
+		this.dueño = dueño;
+	}
+	
+}
