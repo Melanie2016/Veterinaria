2fe069aeb26c35a1fe9026672425a633a42fbf8c
Laush <laush_x4@hotmail.com>
2018-06-25 22:37:06 -0300

    se agrego clases Vacuna y EstadoVacuna

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/EstadoVacuna.java
@@ -0,0 +1,52 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class EstadoVacuna {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String estado; //ok:aplicada  //no: no aplicada
+	
+	@ManyToOne
+	private Vacuna vacuna;
+	
+	@ManyToOne
+	private Mascota mascota;
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Vacuna getVacuna() {
+		return vacuna;
+	}
+	public void setVacuna(Vacuna vacuna) {
+		this.vacuna = vacuna;
+	}
+	public Mascota getMascota() {
+		return mascota;
+	}
+	public void setMascota(Mascota mascota) {
+		this.mascota = mascota;
+	}
+	
+
+}
---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Vacuna.java
@@ -0,0 +1,55 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Vacuna {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombreVacuna;
+	private String animal;
+	private Integer vencimiento; // expresado en meses para calcular al revacunacion
+
+	
+	@ManyToOne
+	//private Vacunacion vacunacion;
+	
+	
+	//getters y setters
+	
+		public Long getId() {
+			return id;
+		}
+
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+		
+		public String getNombreVacuna() {
+			return nombreVacuna;
+		}
+
+
+		public void setNombreVacuna(String nombreVacuna) {
+			this.nombreVacuna = nombreVacuna;
+		}
+		
+		
+		public Integer getVencimiento() {
+			return vencimiento;
+		}
+
+
+		public void setVencimiento(Integer vencimiento) {
+			this.vencimiento = vencimiento;
+		}
+
+}
